6874b8c771f82f80af61fbd622f6021cb7aae870
Josefine Holth <josefine.magnussen@hotmail.com>
2021-01-08 19:54:40 +0100

    small css fixes

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,26 +1,48 @@
 <template>
-  <div class="footer">
-    <div>
-      <img class="logos" src="../assets/gaming-pad.png" />
+  <div style="background-color: var(--textColorHover);">
+    <div class="footer">
+      <div>
+        <img class="logos" src="../assets/gaming-pad.png" />
+      </div>
+      <div class="links">
+        <router-link class="link" to="/">HOME</router-link>
+        <br />
+        <router-link class="NavLink" to="/OurActivitys"
+          >OUR ACTIVITY</router-link
+        >
+        <br />
+
+        <router-link class="NavLink" to="/Register">REGISTER</router-link>
+      </div>
+      <div class="SoMe">
+        <img src="https://img.icons8.com/cotton/64/000000/instagram-new.png" />
+        <img src="https://img.icons8.com/cotton/64/000000/facebook.png" />
+        <img src="https://img.icons8.com/cotton/64/000000/youtube.png" />
+        <img src="https://img.icons8.com/cute-clipart/64/000000/twitch.png" />
+      </div>
     </div>
-    <div class="links">
-      <router-link class="link" to="/">HOME</router-link>
-      <router-link class="NavLink" to="/OurActivitys">OUR ACTIVITY</router-link>
-      <router-link class="NavLink" to="/Register">REGISTER</router-link>
-    </div>
-    <div class="SoMe">
-      <img src="https://img.icons8.com/cotton/64/000000/instagram-new.png" />
-      <img src="https://img.icons8.com/cotton/64/000000/facebook.png" />
-      <img src="https://img.icons8.com/cotton/64/000000/youtube.png" />
-      <img src="https://img.icons8.com/cute-clipart/64/000000/twitch.png" />
+    <div class="linkToPage">
+      <a href="https://josfineholth.tech/">© Holth Development 2020</a>
     </div>
   </div>
 </template>
 <style scoped>
-.footer {  
+.linkToPage {
+  text-align: center;
+  padding-bottom: 30px;
+}
+.linkToPage > a {
+  color: white;
+  font-size: 18px;
+  text-decoration: none;
+}
+.linkToPage:hover {
+  font-size: 18px;
+}
+.footer {
   background-color: var(--textColorHover);
   color: white;
-  padding-top:3%;
+  padding-top: 3%;
   padding: 30px;
 }
 .footer > div {
@@ -29,38 +51,44 @@
 }
 
 .logos {
-  width: 50%;
+    width: 30%;
   display: block;
   margin-left: auto;
   margin-right: auto;
+}
+.links{
+    padding-top: 10%;
+  text-align: center;
+  line-height: 2rem;
+}
 
-}
-.links {
-  display: grid;
-}
 .links > a {
   color: white;
   text-decoration: none;
   text-transform: uppercase;
   font-size: 1.5rem;
 }
+.SoMe{
+display: flex;
+justify-content: center;
+}
 .SoMe > img {
-  width: 20%;
+  width: 10%;
   padding: 2%;
 }
 @media only screen and (min-width: 900px) {
   .footer {
     padding-top: 20px;
     background-color: var(--textColorHover);
-    grid-template-columns:1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
     display: grid;
     color: white;
-    
   }
   .SoMe > img {
-    margin-top:15%;
-    display:inline;
-    width: 90px;
+    margin-top: 10%;
+    display: inline;
+    width: 10%;
+    height: 35%;
     padding: 2px;
   }
 }
